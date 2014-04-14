1007801088a47ee10d38a38c1f2fa7813001ab37
linkin9014 <linkin9014@gmail.com>
2014-04-14 02:57:17 +0000

    avances en consulta

--- SIMS/src/servicio/maestros/SAntecedente.java
+++ SIMS/src/servicio/maestros/SAntecedente.java
@@ -1,6 +1,11 @@
 package servicio.maestros;
 
+import java.util.List;
+
 import interfacedao.maestros.IAntecedenteDAO;
+
+import modelo.maestros.Antecedente;
+import modelo.maestros.Paciente;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +15,20 @@ public class SAntecedente {
 
 	@Autowired
 	private IAntecedenteDAO	antecedenteDAO;
+
+	public List<Antecedente> buscarLaborales() {
+		return antecedenteDAO.findByAntecedenteTipoTipoTrueOrderByAntecedenteTipoNombreAsc();
+	}
+
+	public List<Antecedente> buscarMedicos() {
+		return antecedenteDAO.findByAntecedenteTipoTipoFalseOrderByAntecedenteTipoNombreAsc();
+	}
+
+	public List<Antecedente> buscarLaboralesPaciente(Paciente paciente) {
+		return antecedenteDAO.findByPacientesAndAntecedenteTipoTipoTrueOrderByAntecedenteTipoNombreAsc(paciente);
+	}
+
+	public List<Antecedente> buscarMedicosPaciente(Paciente paciente) {
+		return antecedenteDAO.findByPacientesAndAntecedenteTipoTipoFalseOrderByAntecedenteTipoNombreAsc(paciente);
+	}
 }
