8bb24cea0eb2070cbb8ff5695a79e55329ea06d3
linkin9014@gmail.com <linkin9014@gmail.com@8cd91833-2d6d-564e-ff7e-0917f69717fe>
2014-09-25 21:08:11 +0000

    Funcionalidad para que se pueda ver el valor del inventario en las medicinas (hay q definir bien como se hara la restauracion de las medicinas y los F4101 para poder ajustar bien esto) Añadido mas tiempo por sesion Filtro por especialidad y no por especialista Reportes se generan sin importar el path Reporte de consulta con toda la informacion de especialistas medicinas examenes y estudios

---
+++ SIMS/src/interfacedaoInventario/IF41021DAO.java
@@ -0,0 +1,19 @@
+package interfacedaoInventario;
+
+import java.util.List;
+
+
+
+
+import modelo.inventario.F41021;
+import modelo.inventario.F41021PK;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface IF41021DAO extends JpaRepository<F41021, F41021PK> {
+
+	@Query("select distinct f.id.liitm from F41021 f where f.id.limcu = ?1")
+	List<Double> buscarItemDistintos(String value);
+
+}
