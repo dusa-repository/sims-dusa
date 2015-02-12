257797091e053b60f331bca3fa95a1ac35bd7e6b
linkin9014@gmail.com <linkin9014@gmail.com@8cd91833-2d6d-564e-ff7e-0917f69717fe>
2015-02-12 19:31:47 +0000

    Muestra las medicinas existentes en todas las ubicaciones de la mcu 'Planta' en la consulta OJO: Si se busca la medicina desde el filtro no aparece la cantidad, y la razon es porque el listbox no se renderiza completamente, solo los primeros diez, la unica forma de ver la existencia real es pasando pagina a pagina, en espera de feedbackes jejeje Corregido detalle en grupo

--- SIMS/src/interfacedaoInventario/IF41021DAO.java
+++ SIMS/src/interfacedaoInventario/IF41021DAO.java
@@ -2,9 +2,6 @@ package interfacedaoInventario;
 
 import java.util.List;
 
-
-
-
 import modelo.inventario.F41021;
 import modelo.inventario.F41021PK;
 
@@ -15,5 +12,8 @@ public interface IF41021DAO extends JpaRepository<F41021, F41021PK> {
 
 	@Query("select distinct f.id.liitm from F41021 f where f.id.limcu = ?1")
 	List<Double> buscarItemDistintos(String value);
+
+	@Query("select coalesce(sum(f.lipqoh), '0') from F41021 f where f.id.liitm = ?1 and f.id.limcu = ?2")
+	Double sumByItemAndMcu(Double imitm, String string);
 
 }
