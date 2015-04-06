2fe12a56a55e551069cb93f92e5d19edce199b25
ysol.m15@gmail.com <ysol.m15@gmail.com@8cd91833-2d6d-564e-ff7e-0917f69717fe>
2015-04-06 01:34:51 +0000

    Ejemplo de parte de Visita social para nueva asignacion de Victor

--- SIMS/src/interfacedao/social/IVisitaSocialDAO.java
+++ SIMS/src/interfacedao/social/IVisitaSocialDAO.java
@@ -1,9 +1,15 @@
 package interfacedao.social;
 
+import java.util.List;
+
 import modelo.social.VisitaSocial;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IVisitaSocialDAO  extends JpaRepository<VisitaSocial, Long>{
+
+	List<VisitaSocial> findByIdVisitaStartingWithAllIgnoreCase(String valor);
+
+
 
 }
